76e06a63a5f392cc93291d63bb8d120e5f9c77c5
Shrey <shreybirmiwal@gmail.com>
2021-01-12 20:30:10 -0600

    'initial'

---
+++ Pookarma/routes.py
@@ -0,0 +1,58 @@
+from Pookarma.models import User, Post
+from flask import render_template, url_for, flash, redirect, request
+from Pookarma.forms import RegistrationForm, LoginForm
+from Pookarma import app, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route("/")
+@app.route("/feed")
+def feed():
+    return render_template('feed.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('feed'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password= hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('feed'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('feed'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('feed'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash('Logout success!')
+    return redirect(url_for('feed'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
